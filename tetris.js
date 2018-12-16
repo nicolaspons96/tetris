5e682b20f8375262bf06d14fae8e6999d0a4d318
Nicolas Pons <pons_n@MacBook-Pro-de-Nicolas.local>
2018-12-16 19:42:23 +0100

    tetris.js: created and works with a simple shape

---
+++ tetris.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+context.scale(20, 20);
+
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+];
+
+function draw() {
+    drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value != 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
+    });
+}
+
+const player = {
+    pos: { x: 5, y: 5 },
+    matrix: matrix
+}
+
+draw();
